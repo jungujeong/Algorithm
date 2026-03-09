9a1b8f275df84f8cba7e5aef746ad0f7ee678945
jungujeong <154983099+jungujeong@users.noreply.github.com>
2026-03-10 01:58:24 +0900

    [Bronze III] Title: ACM 호텔, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ Python/백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -3,5 +3,5 @@ T = int(sys.stdin.readline().rstrip())
 for i in range(T):
     H, W, N = map(int, sys.stdin.readline().split())
     floor = N%H if N%H!=0 else H
-    num = (N-1)//H+1 if len(str((N-1)//H+1))==2 else '0'+ str((N-1)//H+1)
-    print(f"{floor}{num}")
+    num = (N-1)//H+1
+    print(f"{floor}{num:02d}")
