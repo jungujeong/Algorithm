cdec39768e8f7e6f6553881bd48f1c47019cef02
jungujeong <154983099+jungujeong@users.noreply.github.com>
2026-04-07 23:25:27 +0900

    [Silver IV] Title: 카드2, Time: 184 ms, Memory: 55928 KB -BaekjoonHub

--- Python/백준/Silver/2164. 카드2/카드2.py
+++ Python/백준/Silver/2164. 카드2/카드2.py
@@ -4,5 +4,5 @@ n : int = int(sys.stdin.readline())
 cards = deque([i for i in range(1, n+1)])
 while (len(cards)>1):
     cards.popleft()
-    cards.append(cards.popleft())
+    cards.rotate(-1)
 print(cards[0])
